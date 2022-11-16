fcb28119315b9ba03c7c29e7ef40cd723c3933f5
yeahzizi <newyeahzizi@gmail.com>
2022-11-16 23:30:01 +0900

    day01_night

--- final_pjt_back/movies/models.py
+++ final_pjt_back/movies/models.py
@@ -25,3 +25,16 @@ class Rate(models.Model):
     rate_user = models.ForeignKey('accounts.User', on_delete=models.CASCADE, related_name='user_rated')
     rate_movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     rate_score = models.FloatField()
+
+class Actor(models.Model):
+    actor_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    overview = models.TextField(null=True, blank=True)
+    profile_path = models.CharField(max_length=200, null=True, blank=True)
+
+class Director(models.Model):
+    director_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    overview = models.TextField(null=True, blank=True)
+    profile_path = models.CharField(max_length=200, null=True, blank=True)
+    

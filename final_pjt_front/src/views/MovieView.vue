95cd2edd50d3c3eec1baae8a6685e49055af10cf
yr7256 <yr7256@gmail.com>
2022-11-19 01:47:43 +0900

    movies

--- final_pjt_front/src/views/MovieView.vue
+++ final_pjt_front/src/views/MovieView.vue
@@ -1,17 +1,18 @@
 <template>
   <div>
     <img class="full" alt="" src="../assets/main.png">
+    <MovieList/>
   </div>
 </template>
 
 <script>
+import MovieList from '@/components/movies/MovieList'
 
 export default {
   name: 'MovieView',
   components: {
-  
-
-  }
+    MovieList
+  },
 }
 </script>
 

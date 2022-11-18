376c677e0ee41a8492176ad2e5b037bb717bd8f1
yr7256 <yr7256@gmail.com>
2022-11-19 02:26:04 +0900

    movielist 출력

--- final_pjt_front/src/components/movies/MovieList.vue
+++ final_pjt_front/src/components/movies/MovieList.vue
@@ -1,16 +1,26 @@
 <template>
   <div>
     <h1>무비리스트</h1>
-    
+    <div v-for="(movie, index) in movies" :key="index">
+      <img :src="MoviePosterurl+`${movie.poster_path}`" alt="">
+      {{ movie.title }}
+    </div>
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'MovieList',
   data() {
     return {
-      movies: this.$store.state.movies
+      movies: this.$store.state.movies,
+      MoviePosterurl: 'https://www.themoviedb.org/t/p/w300_and_h450_bestv2'
+    }
+  },
+  computed: {
+    movieurl() {
+      return this.img_url+this.movie.poster_path
     }
   }
 }

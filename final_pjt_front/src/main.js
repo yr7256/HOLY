280785d96cc5087fa91735dfce4899268729e288
yr7256 <yr7256@gmail.com>
2022-11-19 20:49:54 +0900

    여기서부터

--- final_pjt_front/src/main.js
+++ final_pjt_front/src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
-import GAuth from 'vue-google-oauth2'
 
 Vue.config.productionTip = false
 
@@ -16,10 +15,3 @@ import Meta from 'vue-meta'
 Vue.use(Meta, {
   attribute: 'data-vue-meta'
 })
-
-const gauthOption = {
-  clientId: '1025901526115-cvgcig9fl391aj9ruhoues7onofeghrj.apps.googleusercontent.com.apps.googleusercontent.com',
-  scope: 'profile email',
-  prompt: 'select_account'
-}
-Vue.use(GAuth, gauthOption)

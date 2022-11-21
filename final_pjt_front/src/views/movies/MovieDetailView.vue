68a68b3cfe07dc4bea7fea436fba8f1be4ca9a1d
yeahzizi <newyeahzizi@gmail.com>
2022-11-21 12:20:30 +0900

    day06 세부페이지

--- final_pjt_front/src/views/movies/MovieDetailView.vue
+++ final_pjt_front/src/views/movies/MovieDetailView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="body">
     <h1>{{ movie?.title }}</h1>
-    <!-- <img :src="MoviePosterurl + `${movie.poster_path}`" alt="" /> -->
+    <img :src="MoviePosterurl + `${movie.poster_path}`" @error="noPosterImg" />
     <p>{{ movie?.release_date }}</p>
     <hr />
     <!-- <div v-for="(name, index) in movie.actors" :key="index" > -->
@@ -10,43 +10,71 @@
     </div> -->
     <!-- <p> {{ sameId }} </p> -->
     <!-- </div> -->
+    
+    <!-- <div v-if="actorsLength == actors.length"> -->
 
-    <div v-if="actorsLength == actors.length">
-      <div v-for="(name, index) in actors" :key="index">{{ name }}</div>
-    </div>
-
-    <!-- <div v-for="(name, index) in movie.actors" :key="index">{{ getActorname(name) }} </div> -->
-    <!-- <div v-for="(name, index) in movie.directors" :key="index"> -->
-    <!-- <p>{{ name }}</p> -->
+    <!-- <div class="row row-cols-auto row-cols-md-3 g-2"> -->
+      <div class="col" v-for="(name, index) in directors" :key="index">
+        <div class="card h-80 w-80">
+          <a :href="personDetailurl + `${name.id}`">
+            <img class="card-img-top" style="width:100px; height:100px;" :src="MoviePosterurl+`${name.profile_path}`" alt="" @error="noDirectorImg">
+          </a>
+          <div class="card-body">
+            <p class="card-title">{{ name.name }}</p>
+          </div>
+        </div>
+      </div>
     <!-- </div> -->
-    <hr />
-    <p>{{ movie?.overview }}</p>
-  </div>
+    <br>
+    <div class="row row-cols-auto row-cols-md-3 g-2">
+      <div class="col" v-for="(name, index) in actors" :key="index">
+        <div class="card h-80 w-80">
+          <a :href="personDetailurl + `${name.id}`">
+            <img class="card-img-top" style="width:100px; height:100px;" :src="MoviePosterurl+`${name.profile_path}`" alt="" @error="noImage">
+          </a>
+          <div class="card-body">
+            <p class="card-title">{{ name.name }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+
+      <!-- <div v-for="(name, index) in movie.actors" :key="index">{{ getActorname(name) }} </div> -->
+      <!-- <div v-for="(name, index) in movie.directors" :key="index"> -->
+      <!-- <p>{{ name }}</p> -->
+      <!-- </div> -->
+      <hr />
+      <p>{{ movie?.overview }}</p>
+    
+    </div>
 </template>
 
 <script>
 import axios from "axios";
 const API_URL = "http://127.0.0.1:8000";
+import img from '@/assets/actorimg.png'
+import directorimg from '@/assets/directorimg.png'
+import posterimg from '@/assets/noposterimg.png'
 
 export default {
   name: "MovieDetailView",
   data() {
     return {
       movie: null,
-
       MoviePosterurl: "https://www.themoviedb.org/t/p/w300_and_h450_bestv2",
       actors: [],
       actorsLength: 0,
+      personDetailurl: "https://www.themoviedb.org/person/",
+      directors: []
     };
   },
   created() {
-    this.getMovieDetail();
-  },
-  created() {
-    this.getMovieDetail()
+    this.getActorList();
+    this. getDirectorList();
   },
   methods: {
-    getMovieDetail() {
+    getActorList() {
       axios({
         methods: "get",
         url: `${API_URL}/movies/${this.$route.params.id}/`,
@@ -62,21 +90,54 @@ export default {
               url: `${API_URL}/movies/actors/${actorArr[i]}/`,
             })
               .then((response) => {
-                this.actors.push(response.data.name);
+                this.actors.push(response.data);
               })
               .catch((err) => console.log(err));
           }
         })
-        .catch((err) => console.log(err));
-    },
+      },
+    getDirectorList() {
+      axios({
+        methods: "get",
+        url: `${API_URL}/movies/${this.$route.params.id}/`,
+      })
+        .then((res) => {
+          console.log(res);
+          this.movie = res.data;
+          const directorArr = res.data.directors;
+          this.directorsLength = directorArr.length;
+          for (let i = 0; i < directorArr.length; i++) {
+            axios({
+              methods: "get",
+              url: `${API_URL}/movies/directors/${directorArr[i]}/`,
+            })
+              .then((response) => {
+                this.directors.push(response.data);
+              })
+              .catch((err) => console.log(err));
+            }
+          })  
+        },
+      noImage(e) {
+        e.target.src = img
+        // e.target.src = directorimg
+      },
+      noDirectorImg(e) {
+        e.target.src = directorimg
+      },
+      noPosterImg(e) {
+        e.target.src = posterimg
+      }
+
   },
-};
+}
 </script>
 
 <style>
 .body {
   padding-top: 75px;
   /* 생략 */
+
 }
 
 
